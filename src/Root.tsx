bf693ff1cc477e824744591169d4851e827c96c1
Almagribii <brucadalm@gmail.com>
2025-12-22 22:01:40 +0700

    add counter root

--- src/Root.tsx
+++ src/Root.tsx
@@ -1,11 +1,13 @@
 import { Routes, Route } from "react-router-dom";
 import { UseState } from "./pages/UseState";
+import { Counter } from "./hooks-lab/useState/Counter";
 
 function App() {
   return (
     <div>
        <Routes>
        <Route path="/usestate" element={<UseState />} />
+       <Route path="/usestate/counter" element={<Counter />} />
       </Routes>
     </div>
   );
