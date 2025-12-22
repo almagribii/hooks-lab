9cb6ca500b0cec92a2ed9a85507f594e208bcf2f
Almagribii <brucadalm@gmail.com>
2025-12-22 20:36:52 +0700

    update

--- src/main.tsx
+++ src/main.tsx
@@ -1,11 +1,14 @@
 import { StrictMode } from "react";
 import { createRoot } from "react-dom/client";
+import { BrowserRouter } from "react-router-dom";
 import "./index.css";
-import App from "./App.tsx";
-import MainLayout from "./components/main.tsx";
+import App from "./Root.tsx";
+import MainLayout from "./components/MainLayout.tsx";
 
 createRoot(document.getElementById("root")!).render(
   <StrictMode>
-    <MainLayout children={<App />} />
+    <BrowserRouter>
+      <MainLayout children={<App />} />
+    </BrowserRouter>
   </StrictMode>
 );
