94b55c9651f4fee5ef75defe11f7f517da4f6224
Almagribii <brucadalm@gmail.com>
2025-12-25 21:35:51 +0700

    update root

--- src/Root.tsx
+++ src/Root.tsx
@@ -8,11 +8,18 @@ import { Synchronization } from "./hooks-lab/useEffect/Synchronization";
 import { Timer } from "./hooks-lab/useEffect/Timer";
 import { FetchingData } from "./hooks-lab/useEffect/FetchingData";
 import { EventListener } from "./hooks-lab/useEffect/EventListener";
+import { UseContext } from "./pages/UseContext";
+import { UseCallback } from "./pages/UseCallback";
+import { UseMemo } from "./pages/UseMemo";
+import { UseReducer } from "./pages/UseReducer";
+import { UseRef } from "./pages/UseRef";
+import { Dashboard } from "./pages/Dashboard";
 
 function App() {
   return (
     <div>
        <Routes>
+        <Route path="/" element={<Dashboard/>} />
         {/* Use State Case */}
        <Route path="/usestate" element={<UseState />} />
        <Route path="/usestate/counter" element={<Counter />} />
@@ -25,6 +32,15 @@ function App() {
        <Route path="/useeffect/timer" element={<Timer />} />
        <Route path="/useeffect/fetchingdata" element={<FetchingData />} />
        <Route path="/useeffect/eventlistener" element={<EventListener />} />
+
+
+       <Route path="/usecontext" element={<UseContext />} />
+       <Route path="/usecallback" element={<UseCallback/>} />
+       <Route path="/usememo" element={<UseMemo/>} />
+       <Route path="/usereducer" element={<UseReducer/>} />
+       <Route path="/useref" element={<UseRef/>} />
+
+
       </Routes>
     </div>
   );
