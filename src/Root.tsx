099ad438bbedf852e16d8c4707c80dccd791d513
Almagribii <brucadalm@gmail.com>
2025-12-26 22:43:11 +0700

    fixed root include add UseContext root

--- src/Root.tsx
+++ src/Root.tsx
@@ -15,8 +15,8 @@ import { UseReducer } from "./pages/UseReducer";
 import { UseRef } from "./pages/UseRef";
 import { Dashboard } from "./pages/Dashboard";
 import { DarkLightMode } from "./hooks-lab/useContext/DarkLightMode";
-import { AuthContext } from "./hooks-lab/useContext/AuthContext";
-import { LanguageMode } from "./hooks-lab/useContext/Language";
+import { AuthLab } from "./hooks-lab/useContext/AuthLab";
+import { LanguageMode } from "./hooks-lab/useContext/LanguageMode";
 
 function App() {
   return (
@@ -42,7 +42,7 @@ function App() {
         {/* Use Context Case */}
         <Route path="/usecontext" element={<UseContext />} />
         <Route path="/usecontext/darklightmode" element={<DarkLightMode />} />
-        <Route path="/usecontext/authcontext" element={<AuthContext />} />
+        <Route path="/usecontext/authcontext" element={<AuthLab />} />
         <Route path="/usecontext/languagemode" element={<LanguageMode />} />
 
         {/* Other Hooks Cases */}
